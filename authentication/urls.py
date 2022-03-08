ddc897fe3ce41a7be9aed58979613e58113a79ed
Adiboy <adiboy_3112@kgpian.iitkgp.ac.in>
2022-03-08 16:20:55 +0530

    Updated forgot Password

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+from django.contrib.auth import views as auth_views 
 urlpatterns = [
     
     path('', views.home, name='home'),
@@ -11,4 +11,8 @@ urlpatterns = [
     path('signout', views.signout, name='signout'),
     path('activate/<uidb64>/<token>',  
         views.activate, name='activate'),  
-]
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),      
+    ]
