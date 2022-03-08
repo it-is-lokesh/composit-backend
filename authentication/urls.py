3e165332066507f88fc0e12a477c2f36b7bbeafc
Adiboy <adiboy_3112@kgpian.iitkgp.ac.in>
2022-03-08 18:01:01 +0530

    Working Reset Password API

--- authentication/urls.py
+++ authentication/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('signout', views.signout, name='signout'),
     path('activate/<uidb64>/<token>',  
         views.activate, name='activate'),  
-    path("password_reset", views.password_reset_request, name="password_reset"),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),      
+    # path("password_reset", views.password_reset_request, name="password_reset"),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),      
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     ]
--- authentication/views.py
+++ authentication/views.py
@@ -14,11 +14,6 @@ from authentication.decrypter import decoder
 from django.core.mail.backends.smtp import EmailBackend
 
 
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_str
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from .token import account_activation_token
-
 
 def home(request):
     return render(request, "authentication/index.html")
@@ -110,33 +105,7 @@ def signup(request):
             print("2", context)
             return Response(context)
     return Response({'fail': 'true'})
-def password_reset_request(request):
-    if request.method == "POST":
-        password_reset_form = PasswordResetForm(request.POST)
-        if password_reset_form.is_valid():
-            data = password_reset_form.cleaned_data['email']
-            associated_users = User.objects.filter(Q(email=data))
-            if associated_users.exists():
-                for user in associated_users:
-                    subject = "Password Reset Requested"
-                    email_template_name = "main/password/password_reset_email.txt"
-                    c = {
-                    "email":user.email,
-                    'domain':'127.0.0.1:8000',
-                    'site_name': 'Website',
-                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                    "user": user,
-                    'token': default_token_generator.make_token(user),
-                    'protocol': 'http',
-                    }
-                    email = render_to_string(email_template_name, c)
-                    try:
-                        send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
-                    except BadHeaderError:
-                        return HttpResponse('Invalid header found.')
-                    return redirect ("/password_reset/done/")
-    password_reset_form = PasswordResetForm()
-    return render(request=request, template_name="main/password/password_reset.html", context={"password_reset_form":password_reset_form})
+
 
 def activate(request, uidb64, token):
     try:
