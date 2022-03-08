b36938c23a96bb4b0172faa4a76ad0d3e1313a81
Lokesh <g.sailokesh9@gmail.com>
2022-03-08 12:54:20 +0530

    changes cities

--- composit/settings.py
+++ composit/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'composit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "/templates/authentication")],
+        'DIRS': [os.path.join(BASE_DIR, "/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
