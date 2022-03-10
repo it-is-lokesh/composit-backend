b3e9f56ba7596d925d5e799b7dc835876fee4fd8
Lokesh <g.sailokesh9@gmail.com>
2022-03-10 23:58:15 +0530

    c

--- userdashboard/views.py
+++ userdashboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import userdashboard
+from userdashboard.models import userDashboard
 
 # Create your views here.
+
+def registerForEvent(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        eventID = request.data['eventID']
+
+        myuser = userdashboard.objects.all().filter(username=username)
+        myuser.events_registered = myuser.events_registered + eventID  + ' '       
