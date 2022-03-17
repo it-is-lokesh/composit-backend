29bc9047831f86d67794395728d943ef505db6d5
Lokesh <g.sailokesh9@gmail.com>
2022-03-17 21:03:14 +0530

    signin changed

--- userdashboard/views.py
+++ userdashboard/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
 from httplib2 import Response
 import userdashboard
-from userdashboard.models import userDashboard
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def registerForEvent(request):
+    if request.method == 'GET':
+        return HttpResponse("Not authorised")
+
     if request.method == 'POST':
         username = request.data['username']
         eventID = request.data['eventID']
 
         myuser = userdashboard.objects.all().filter(username=username)
         myuser.events_registered = myuser.events_registered + eventID + ' '
+        print(myuser.events_registered)
         context = {'success': 1}
+        context = json.dumps(context)
         return Response(context)
